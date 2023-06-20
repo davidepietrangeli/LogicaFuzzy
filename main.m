94942c134d2029d4d9cc6b21ab6a4f59ffbffb24
Davide Pietrangeli <93650615+davidepietrangeli@users.noreply.github.com>
2023-06-20 18:56:08 +0200

    commit bozza

--- main.m
+++ main.m
@@ -3,10 +3,11 @@ fis = readfis('sistema_fuzzy.fis');
 
 % Definisci i valori di input
 posizione_classifica_piloti = 15;
-stato_di_forma = 3;
+stato_di_forma = 1;
 vittorie_stagionali = 2;
 infortunio_pilota = 1;
 cadute_stagionali = 2;
+classifica_marca_piloti =1;
 
 % Valori di input come vettore
 input = [posizione_classifica_piloti stato_di_forma vittorie_stagionali infortunio_pilota cadute_stagionali];
