a7b49d9cf6c48449232db2f001a4bae80a960287
Davide Pietrangeli <93650615+davidepietrangeli@users.noreply.github.com>
2023-06-21 17:39:16 +0200

    commit

--- main.m
+++ main.m
@@ -1,16 +1,16 @@
 % Carica il sistema fuzzy dal file separato
-fis = readfis('sistema_fuzzy.fis');
+fis = readfis('prova.fis');
 
 % Definisci i valori di input
 posizione_classifica_piloti = 15;
 stato_di_forma = 1;
 vittorie_stagionali = 2;
-infortunio_pilota = 1;
+salute_pilota = 1;
 cadute_stagionali = 2;
-classifica_marca_piloti =1;
+classifica_marca_moto =1;
 
 % Valori di input come vettore
-input = [posizione_classifica_piloti stato_di_forma vittorie_stagionali infortunio_pilota cadute_stagionali];
+input = [posizione_classifica_piloti stato_di_forma vittorie_stagionali salute_pilota cadute_stagionali classifica_marca_moto];
 
 % Esegui la valutazione del sistema fuzzy
 output = evalfis(input, fis);
